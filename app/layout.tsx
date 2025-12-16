ab28876c7ea362bb51d98327b73c18ad7d5ae0c2
masonba11 <masonba11@users.noreply.github.com>
2025-12-16 02:59:05 -0700

    Add Google Ads conversion tracking (gtag.js)

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17795508639"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17795508639');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(combinedSchema) }}
